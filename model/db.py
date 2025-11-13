0fb7f362eb642435fb2420babdffa64bca4fa914
RICHARD_LENOVO\orgal <orgalorg8732@gmail.com>
2025-11-13 22:24:39 +0800

    upload

---
+++ model/db.py
@@ -0,0 +1,19 @@
+from psycopg_pool import AsyncConnectionPool
+from psycopg.rows import dict_row
+
+DATABASE_URL = "postgresql://neondb_owner:npg_UJSRW7QLnji3@ep-summer-cake-a1owtkqw-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
+
+
+_pool: AsyncConnectionPool | None = None
+
+async def getDB():
+    global _pool
+    if _pool is None:
+        _pool = AsyncConnectionPool(
+            conninfo = DATABASE_URL,
+            kwargs={"row_factory": dict_row},
+            open= False
+        )
+        await _pool.open()
+    async with _pool.connection() as conn:
+        yield conn
